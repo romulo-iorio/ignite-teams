f67953f3463329689d7b37700cb3cca948d0c524
Romulo Iorio <romuloiorio@hotmail.com>
2024-02-22 19:06:11 -0300

    feat: creating useRoutes custom hook to allow navigation agnostically to the library used for navigation

--- src/routes/useRoutes.ts
+++ src/routes/useRoutes.ts
@@ -1,11 +1,12 @@
 import { useNavigation } from "@react-navigation/native";
 
 export const useRoutes = () => {
-  const { navigate } = useNavigation();
+  const { navigate, goBack: navigateToPreviousScreen } = useNavigation();
 
   return {
     navigateToPlayers: (groupId: string) => navigate("players", { groupId }),
     navigateToGroups: () => navigate("groups"),
     navigateToNewGroup: () => navigate("new-group"),
+    navigateToPreviousScreen,
   };
 };
