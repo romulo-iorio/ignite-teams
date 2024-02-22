09118bdf891ff15a09bacbf02f82e1d05206d477
Romulo Iorio <romuloiorio@hotmail.com>
2024-02-22 18:46:26 -0300

    feat: creating navigation routes component

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,23 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Groups, NewGroup, Players } from "@screens";
+
+const { Navigator, Screen } = createNativeStackNavigator();
+
+interface ScreenProps {
+  name: string;
+  component: React.FC;
+}
+
+const screens: ScreenProps[] = [
+  { name: "groups", component: Groups },
+  { name: "players", component: Players },
+  { name: "new-group", component: NewGroup },
+];
+
+export const AppRoutes: React.FC = () => {
+  const renderScreens = screens.map((screen) => (
+    <Screen key={screen.name} name={screen.name} component={screen.component} />
+  ));
+
+  return <Navigator>{renderScreens}</Navigator>;
+};
