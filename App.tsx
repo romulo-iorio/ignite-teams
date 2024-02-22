89bb098eb2614eb63513ee24f3cfc74a1eb587c6
Romulo Iorio <romuloiorio@hotmail.com>
2024-02-21 22:18:36 -0300

    feat: creating Loading component

--- App.tsx
+++ App.tsx
@@ -3,17 +3,11 @@ import {
   Roboto_700Bold,
   useFonts,
 } from "@expo-google-fonts/roboto";
-import { ActivityIndicator, View } from "react-native";
 import { ThemeProvider } from "styled-components";
 import { Groups } from "@screens";
 
 import theme from "./src/theme";
-
-const Loading: React.FC = () => (
-  <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-    <ActivityIndicator />
-  </View>
-);
+import { Loading } from "@components";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
