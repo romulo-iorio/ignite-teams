4eed54d698a49f4e273f771464ebc19fd5c30e5b
Romulo Iorio <romuloiorio@hotmail.com>
2024-02-22 20:38:06 -0300

    feat: creating AppError to control exceptions

--- src/storage/group/createGroup.ts
+++ src/storage/group/createGroup.ts
@@ -1,6 +1,7 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import { GROUP_COLLECTION } from "@storage/storageConfig";
+import { AppError } from "@utils/AppError";
 import { Group } from "src/@types/Group";
 
 import { getAllGroups } from "./getAllGroups";
@@ -11,6 +12,13 @@ const generateRandomId = () => {
 
 export const createGroup = async (newGroupName: string) => {
   const groups = await getAllGroups();
+
+  const groupAlreadyExists = groups.find(
+    (group) => group.name === newGroupName
+  );
+
+  if (groupAlreadyExists)
+    throw new AppError("Já existe um grupo cadastrado com esse nome.");
 
   const newGroup: Group = {
     createdAt: new Date().getTime(),
