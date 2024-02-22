fc75428c2294675dbec5ec38ff9d7ae57b6f478d
Romulo Iorio <romuloiorio@hotmail.com>
2024-02-21 23:03:29 -0300

    feat: creating state for dinamically render list of groups using a FlatList

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,15 +1,33 @@
+import { useState } from "react";
+
 import { Highlight, Header, GroupCard } from "@components";
 
 import { Container } from "./styles";
+import { FlatList } from "react-native";
+
+interface GroupProps {
+  groupName: string;
+  id: string;
+}
 
 export const Groups = () => {
+  const [groups, setGroups] = useState<GroupProps[]>([
+    { id: "1", groupName: "Galera do Ignite" },
+    { id: "2", groupName: "Galera do React" },
+    { id: "3", groupName: "Galera do Node" },
+  ]);
+
   return (
     <Container>
       <Header />
 
       <Highlight title="Turmas" subTitle="Jogue com a sua turma" />
 
-      <GroupCard title="Galera do Ignite" />
+      <FlatList
+        renderItem={({ item }) => <GroupCard title={item.groupName} />}
+        keyExtractor={(item) => item.id}
+        data={groups}
+      />
     </Container>
   );
 };
