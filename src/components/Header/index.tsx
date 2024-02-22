c822063495507c2db962c65335736f3e1c5f2d73
Romulo Iorio <romuloiorio@hotmail.com>
2024-02-22 19:13:37 -0300

    feat: linking navigateToGroups function to back button on header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,19 @@
 import logoSrc from "@assets/logo.png";
 
 import { BackButton, BackIcon, Container, Logo } from "./styles";
+import { useRoutes } from "@routes/useRoutes";
 
 interface Props {
   showBackButton?: boolean;
 }
 
 export const Header: React.FC<Props> = ({ showBackButton = false }) => {
+  const { navigateToGroups } = useRoutes();
+
   return (
     <Container showBackButton={showBackButton}>
       {showBackButton && (
-        <BackButton>
+        <BackButton onPress={navigateToGroups}>
           <BackIcon />
         </BackButton>
       )}
