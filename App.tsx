2d0bb819d3495a26b51b0e83f9322972f7880783
Romulo Iorio <romuloiorio@hotmail.com>
2024-02-21 22:04:34 -0300

    feat: using theme stylon Container styling

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { ThemeProvider } from "styled-components";
 import { Groups } from "@screens";
 
-import theme from "@theme";
+import theme from "./src/theme";
 
 export default function App() {
   return (
