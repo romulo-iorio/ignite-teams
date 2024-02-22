a12ebedcf35c68be40f9c5cab588c6993b01ecf4
Romulo Iorio <romuloiorio@hotmail.com>
2024-02-21 22:20:21 -0300

    feat: adding and styling status bar

--- App.tsx
+++ App.tsx
@@ -4,16 +4,24 @@ import {
   useFonts,
 } from "@expo-google-fonts/roboto";
 import { ThemeProvider } from "styled-components";
+import { StatusBar } from "react-native";
+
+import { Loading } from "@components";
 import { Groups } from "@screens";
 
 import theme from "./src/theme";
-import { Loading } from "@components";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        backgroundColor="transparent"
+        barStyle="light-content"
+        translucent
+      />
+
       {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
