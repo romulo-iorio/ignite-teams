83738479f0a0231c11ae61a44dec04456a0776ba
Romulo Iorio <romuloiorio@hotmail.com>
2024-02-22 21:53:26 -0300

    feat: using a loading indicator on groups list

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,7 +2,14 @@ import { useFocusEffect } from "@react-navigation/native";
 import { Alert, FlatList } from "react-native";
 import { useCallback, useState } from "react";
 
-import { Highlight, Header, GroupCard, ListEmpty, Button } from "@components";
+import {
+  Highlight,
+  Header,
+  GroupCard,
+  ListEmpty,
+  Button,
+  Loading,
+} from "@components";
 import { getAllGroups } from "@storage/group";
 import { useRoutes } from "@routes/useRoutes";
 import { Group } from "src/@types/Group";
@@ -10,8 +17,11 @@ import { Group } from "src/@types/Group";
 import { Container } from "./styles";
 
 const fetchGroups = async (
-  setGroups: React.Dispatch<React.SetStateAction<Group[]>>
+  setGroups: React.Dispatch<React.SetStateAction<Group[]>>,
+  setIsLoading: React.Dispatch<React.SetStateAction<boolean>>
 ) => {
+  setIsLoading(true);
+
   try {
     const groups = await getAllGroups();
     const orderedGroups = groups.sort((a, b) => b.createdAt - a.createdAt);
@@ -20,16 +30,19 @@ const fetchGroups = async (
     console.error(error);
     Alert.alert("Turmas", "Não foi possível carregar as turmas.");
   }
+
+  setIsLoading(false);
 };
 
 export const Groups = () => {
   const { navigateToNewGroup, navigateToPlayers } = useRoutes();
 
+  const [isLoading, setIsLoading] = useState(true);
   const [groups, setGroups] = useState<Group[]>([]);
 
   useFocusEffect(
     useCallback(() => {
-      fetchGroups(setGroups);
+      fetchGroups(setGroups, setIsLoading);
     }, [])
   );
 
@@ -41,20 +54,24 @@ export const Groups = () => {
 
       <Highlight title="Turmas" subTitle="Jogue com a sua turma" />
 
-      <FlatList
-        ListEmptyComponent={() => (
-          <ListEmpty message="Nenhuma turma encontrada. Que tal cadastrar a primeira turma?" />
-        )}
-        contentContainerStyle={groups.length === 0 && { flex: 1 }}
-        renderItem={({ item }) => (
-          <GroupCard
-            onPress={() => handleOpenGroup(item.name)}
-            title={item.name}
-          />
-        )}
-        keyExtractor={(item) => item.id}
-        data={groups}
-      />
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <FlatList
+          ListEmptyComponent={() => (
+            <ListEmpty message="Nenhuma turma encontrada. Que tal cadastrar a primeira turma?" />
+          )}
+          contentContainerStyle={groups.length === 0 && { flex: 1 }}
+          renderItem={({ item }) => (
+            <GroupCard
+              onPress={() => handleOpenGroup(item.name)}
+              title={item.name}
+            />
+          )}
+          keyExtractor={(item) => item.id}
+          data={groups}
+        />
+      )}
 
       <Button title="Criar nova turma" onPress={navigateToNewGroup} />
     </Container>
